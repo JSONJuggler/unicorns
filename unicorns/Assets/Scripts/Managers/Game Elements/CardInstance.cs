c27fb8415af2ee07238d63e48714bfab396b5cff
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-06-07 10:28:01 -0500

    mouse hover detection

---
+++ unicorns/Assets/Scripts/Managers/Game Elements/CardInstance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace unicorn
+{
+    public class CardInstance : MonoBehaviour, IClickable
+    {
+        public void OnClick()
+        {
+            Debug.Log(this.gameObject.name);
+        }
+
+        public void OnHighlight()
+        {
+            Debug.Log(this.gameObject.name);
+            Vector3 s = Vector3.one * 2;
+            this.transform.localScale = s;
+        }
+    }
+}
---
+++ unicorns/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using unicorn.GameStates;
+
+namespace unicorn
+{
+    public class GameManager : MonoBehaviour
+    {
+        public State currentState;
+
+        private void Update()
+        {
+            {
+                currentState.Tick(Time.deltaTime);
+            }
+        }
+    }
+}
